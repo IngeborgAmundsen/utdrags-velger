4e6dfeb18ddfc83e4c00c129265fdaa5c597e68f
IngeborgAmundsen <143869388+IngeborgAmundsen@users.noreply.github.com>
2023-10-02 09:52:17 +0200

    Add files via upload

---
+++ egen_oppg.py
@@ -0,0 +1,57 @@
+# Dette er et program som jeg har lagd til kjæresten min, som han har behov for i forbindelse med et prøvespill.
+# Jeg ba han skrive ned alle utdrag som han må øve på (utdragene er korte, mindre deler fra f.eks. en symfoni,
+# der juryen velger tilfeldige utdrag) og jeg kopierte de til
+
+import random
+
+with open("oblig5/utdrag.txt") as file:
+    content = file.readlines()
+    list_excerpts = []
+    for utdrag in content:
+        utdrag = utdrag.replace("\n", "")
+        list_excerpts.append(utdrag)
+
+print("Hei og velkommen til utdrags-velgeren!")
+print()
+
+# printer ascii-art av en klarinett:
+print("""⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⢸⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠻⠻⣤⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠘⠋⣠⣶⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠙⢡⣾⣓⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢤⡙⠣⡶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢦⣬⡭⠶⢲⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠖⢛⣩⣍⠳⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠱⣾⢳⣌⠳⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣯⣿⡿⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⣠⠞⢁⣤⣀⣀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⣿⣿⡿⠛⣡⡾⠃⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠏⣠⣾⠟⠁⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡾⠛⠁⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀""")
+print()
+
+program_kjører = True
+
+while program_kjører:
+
+    antall_utdrag = len(list_excerpts)
+
+    if antall_utdrag == 0:
+        print("Du er ferdig med lista!! Bra jobba 💪💪")
+        program_kjører = False
+
+    elif antall_utdrag != 0:
+        antall = int(input("Hvor mange utdrag vil du spille? "))
+        print()
+
+        for utdrag in range(0, antall):
+            utdrag = random.randint(0, len(list_excerpts)-1)
+            utvalgte_utdrag = list_excerpts[utdrag]
+            print(utvalgte_utdrag)
+
+            list_excerpts.remove(utvalgte_utdrag)
+        print()
+        print(f"Antall utdrag igjen: {len(list_excerpts)}")
+    
